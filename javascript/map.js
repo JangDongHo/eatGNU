938454c5ee35f9dbf23deca52728d4e17f3b3d0b
Jang-DongHo <donghoo18@naver.com>
2022-02-15 19:51:48 +0900

    big test

--- javascript/map.js
+++ javascript/map.js
@@ -1,153 +1,16 @@
 var map = new naver.maps.Map("map", {
   center: new naver.maps.LatLng(37.3595316, 127.1052133),
   zoom: 15,
-  mapTypeControl: true
-});
-
-var infoWindow = new naver.maps.InfoWindow({
-  anchorSkew: true
 });
 
 map.setCursor('pointer');
-
-function searchAddressToCoordinate(address) {
-  naver.maps.Service.geocode({
-      query: address
-  }, function(status, response) {
-      if (status === naver.maps.Service.Status.ERROR) {
-          return alert('Something Wrong!222');
-      }
-
-      if (response.v2.meta.totalCount === 0) {
-          return alert('totalCount' + response.v2.meta.totalCount);
-      }
-
-      var htmlAddresses = [],
-          item = response.v2.addresses[0],
-          point = new naver.maps.Point(item.x, item.y);
-
-      if (item.roadAddress) {
-          htmlAddresses.push('[도로명 주소] ' + item.roadAddress);
-      }
-
-      if (item.jibunAddress) {
-          htmlAddresses.push('[지번 주소] ' + item.jibunAddress);
-      }
-
-      if (item.englishAddress) {
-          htmlAddresses.push('[영문명 주소] ' + item.englishAddress);
-      }
-
-      infoWindow.setContent([
-          '<div style="padding:10px;min-width:200px;line-height:150%;">',
-          '<h4 style="margin-top:5px;">검색 주소 : '+ address +'</h4><br />',
-          htmlAddresses.join('<br />'),
-          '</div>'
-      ].join('\n'));
-
-      map.setCenter(point);
-      infoWindow.open(map, point);
-  });
-}
 
 function initGeocoder() {
   if (!map.isStyleMapReady) {
       return;
   }
 
-  map.addListener('click', function(e) {
-      searchCoordinateToAddress(e.coord);
-  });
-
-  $('#address').on('keydown', function(e) {
-      var keyCode = e.which;
-
-      if (keyCode === 13) { // Enter Key
-          searchAddressToCoordinate($('#address').val());
-      }
-  });
-
-  $('#submit').on('click', function(e) {
-      e.preventDefault();
-
-      searchAddressToCoordinate($('#address').val());
-  });
-
   searchAddressToCoordinate('가좌길 71');
-}
-
-function makeAddress(item) {
-  if (!item) {
-      return;
-  }
-
-  var name = item.name,
-      region = item.region,
-      land = item.land,
-      isRoadAddress = name === 'roadaddr';
-
-  var sido = '', sigugun = '', dongmyun = '', ri = '', rest = '';
-
-  if (hasArea(region.area1)) {
-      sido = region.area1.name;
-  }
-
-  if (hasArea(region.area2)) {
-      sigugun = region.area2.name;
-  }
-
-  if (hasArea(region.area3)) {
-      dongmyun = region.area3.name;
-  }
-
-  if (hasArea(region.area4)) {
-      ri = region.area4.name;
-  }
-
-  if (land) {
-      if (hasData(land.number1)) {
-          if (hasData(land.type) && land.type === '2') {
-              rest += '산';
-          }
-
-          rest += land.number1;
-
-          if (hasData(land.number2)) {
-              rest += ('-' + land.number2);
-          }
-      }
-
-      if (isRoadAddress === true) {
-          if (checkLastString(dongmyun, '면')) {
-              ri = land.name;
-          } else {
-              dongmyun = land.name;
-              ri = '';
-          }
-
-          if (hasAddition(land.addition0)) {
-              rest += ' ' + land.addition0.value;
-          }
-      }
-  }
-
-  return [sido, sigugun, dongmyun, ri, rest].join(' ');
-}
-
-function hasArea(area) {
-  return !!(area && area.name && area.name !== '');
-}
-
-function hasData(data) {
-  return !!(data && data !== '');
-}
-
-function checkLastString (word, lastString) {
-  return new RegExp(lastString + '$').test(word);
-}
-
-function hasAddition (addition) {
-  return !!(addition && addition.value);
 }
 
 naver.maps.onJSContentLoaded = initGeocoder;
