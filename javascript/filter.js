900753b289d893383a59cdda01c92c003ed65489
Jang-DongHo <donghoo18@naver.com>
2022-02-15 15:45:03 +0900

    test

---
+++ javascript/filter.js
@@ -0,0 +1,21 @@
+const tagBtnContainer = document.querySelector('.tag-filter__tags');
+const restaurantContainer = document.querySelector('.restaurant-lists');
+const restaurants = document.querySelectorAll('.restaurant-list');
+tagBtnContainer.addEventListener('click', () => {
+  const filter = 
+  event.target.dataset.filter || event.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+  restaurantContainer.classList.add('anim-out');
+  setTimeout(() => {
+    restaurants.forEach((restaurant) => {
+      if (filter === '*' || filter === restaurant.dataset.type) {
+        restaurant.classList.remove('invisible');
+      } else {
+        restaurant.classList.add('invisible');
+      }
+    });
+    restaurantContainer.classList.remove('anim-out');
+  }, 280);
+});
