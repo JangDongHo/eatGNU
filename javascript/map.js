a2358db48b1a450e249fca7f92c1f03d81534533
Jang-DongHo <donghoo18@naver.com>
2022-02-15 21:01:36 +0900

    새로운 방식 test

--- javascript/map.js
+++ javascript/map.js
@@ -1,17 +1,44 @@
-var map = new naver.maps.Map("map", {
-  center: new naver.maps.LatLng(37.5666103, 126.9783882),
-  zoom: 16
-}),
-infoWindow = null;
-
-function initGeocoder() {
-if (!map.isStyleMapReady) {
-  return;
+function mapGenerator(la,lo,name,link){
+  var HOME_PATH = window.HOME_PATH || '.';
+  var location = new naver.maps.LatLng(la,lo),
+      map = new naver.maps.Map('map', {
+          center: location,
+          zoom: 30
+      }),
+      marker = new naver.maps.Marker({
+          map: map,
+          position: location
+      });
+  
+  var contentString = [
+          '<div class="iw_inner ridi">',
+          '   <h5>'+name+'</h5>',
+          '   <p>네이버 지도 바로 가기<br>',
+          '       <a href="'+link+'" target="_blank">'+link+'/</a>',
+          '   </p>',
+          '</div>'
+      ].join('');
+  
+  var infowindow = new naver.maps.InfoWindow({
+      content: contentString,
+      maxWidth: 300,
+      backgroundColor: "#eee",
+      borderColor: "#A4A4A4",
+      borderRadius:"30px",
+      borderWidth: 2,
+      disableAnchor:true,
+      anchorColor: "#A4A4A4",
+      pixelOffset: new naver.maps.Point(10, -10)
+  });
+  
+  naver.maps.Event.addListener(marker, "click", function(e) {
+      if (infowindow.getMap()) {
+          infowindow.close();
+      } else {
+          infowindow.open(map, marker);
+      }
+  });
+  setTimeout(function() {
+      window.dispatchEvent(new Event('resize'));
+  }, 600);
 }
-
-var latlng = map.getCenter();
-
-console.log(latlng.toString());
-
-naver.maps.onJSContentLoaded = initGeocoder;
-naver.maps.Event.once(map, 'init_stylemap', initGeocoder);
