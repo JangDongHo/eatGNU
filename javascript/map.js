46590c6984d227edac1997d4af5c62ade11af670
Jang-DongHo <donghoo18@naver.com>
2022-02-15 19:34:43 +0900

    test

--- javascript/map.js
+++ javascript/map.js
@@ -10,50 +10,12 @@ var infoWindow = new naver.maps.InfoWindow({
 
 map.setCursor('pointer');
 
-function searchCoordinateToAddress(latlng) {
-
-  infoWindow.close();
-
-  naver.maps.Service.reverseGeocode({
-      coords: latlng,
-      orders: [
-          naver.maps.Service.OrderType.ADDR,
-          naver.maps.Service.OrderType.ROAD_ADDR
-      ].join(',')
-  }, function(status, response) {
-      if (status === naver.maps.Service.Status.ERROR) {
-          return alert('Something Wrong!');
-      }
-
-      var items = response.v2.results,
-          address = '',
-          htmlAddresses = [];
-
-      for (var i=0, ii=items.length, item, addrType; i<ii; i++) {
-          item = items[i];
-          address = makeAddress(item) || '';
-          addrType = item.name === 'roadaddr' ? '[도로명 주소]' : '[지번 주소]';
-
-          htmlAddresses.push((i+1) +'. '+ addrType +' '+ address);
-      }
-
-      infoWindow.setContent([
-          '<div style="padding:10px;min-width:200px;line-height:150%;">',
-          '<h4 style="margin-top:5px;">검색 좌표</h4><br />',
-          htmlAddresses.join('<br />'),
-          '</div>'
-      ].join('\n'));
-
-      infoWindow.open(map, latlng);
-  });
-}
-
 function searchAddressToCoordinate(address) {
   naver.maps.Service.geocode({
       query: address
   }, function(status, response) {
       if (status === naver.maps.Service.Status.ERROR) {
-          return alert('Something Wrong!');
+          return alert('Something Wrong!222');
       }
 
       if (response.v2.meta.totalCount === 0) {
