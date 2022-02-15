8123acf7f092616df17c1f2d5f37f40a08b43bca
Jang-DongHo <donghoo18@naver.com>
2022-02-15 20:36:53 +0900

    test

--- javascript/map.js
+++ javascript/map.js
@@ -10,27 +10,11 @@ if (!map.isStyleMapReady) {
 }
 
 var latlng = map.getCenter();
-var utmk = naver.maps.TransCoord.fromLatLngToUTMK(latlng); // 위/경도 -> UTMK
-var tm128 = naver.maps.TransCoord.fromUTMKToTM128(utmk);   // UTMK -> TM128
-var naverCoord = naver.maps.TransCoord.fromTM128ToNaver(tm128); // TM128 -> NAVER
-
-infoWindow = new naver.maps.InfoWindow({
-  content: ''
-});
 
 map.addListener('click', function(e) {
   var latlng = e.coord,
-      utmk = naver.maps.TransCoord.fromLatLngToUTMK(latlng),
-      tm128 = naver.maps.TransCoord.fromUTMKToTM128(utmk),
-      naverCoord = naver.maps.TransCoord.fromTM128ToNaver(tm128);
-
-  utmk.x = parseFloat(utmk.x.toFixed(1));
-  utmk.y = parseFloat(utmk.y.toFixed(1));
 
   console.log('LatLng: ' + latlng.toString());
-  console.log('UTMK: ' + utmk.toString());
-  console.log('TM128: ' + tm128.toString());
-  console.log('NAVER: ' + naverCoord.toString());
 });
 }
 
