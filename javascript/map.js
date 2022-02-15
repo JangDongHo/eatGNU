fe0980858bad3f6f11b50721b44916b0d1751466
Jang-DongHo <donghoo18@naver.com>
2022-02-15 15:49:33 +0900

    test

--- javascript/map.js
+++ javascript/map.js
@@ -1,19 +1,3 @@
-var mapOptions = {
-  center: new naver.maps.LatLng(37.3595704, 127.105399),
-  zoom: 10
-};
+var mapDiv = document.getElementById("map");
 
-var map = new naver.maps.Map('map', mapOptions);
-
-naver.maps.Service.geocode({
-  address: '불정로 6'
-}, function(status, response) {
-  if (status !== naver.maps.Service.Status.OK) {
-      return alert('Something wrong!');
-  }
-
-  var result = response.result, // 검색 결과의 컨테이너
-      items = result.items; // 검색 결과의 배열
-
-  console.log(items);
-});
+var map = new naver.maps.Map(mapDiv);
