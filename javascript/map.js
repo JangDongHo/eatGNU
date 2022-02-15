3ae8454e049b8d18597efeb000de57e81013bd75
Jang-DongHo <donghoo18@naver.com>
2022-02-15 20:18:19 +0900

    test

--- javascript/map.js
+++ javascript/map.js
@@ -1,28 +1,47 @@
 var map = new naver.maps.Map("map", {
-  center: new naver.maps.LatLng(37.3595316, 127.1052133),
-  zoom: 15,
-});
-
-map.setCursor('pointer');
-
-function searchAddressToCoordinate(address) {
-  naver.maps.Service.geocode({
-      query: address
-  }, function() {
-    console.log("실행");
-    item = response.v2.addresses[0],
-    point = new naver.maps.Point(item.x, item.y);
-
-    map.setCenter(point);
-  });
-}
+  center: new naver.maps.LatLng(37.5666103, 126.9783882),
+  zoom: 16
+}),
+infoWindow = null;
 
 function initGeocoder() {
-  if (!map.isStyleMapReady) {
-      return;
-  }
+if (!map.isStyleMapReady) {
+  return;
+}
 
-  searchAddressToCoordinate('가좌길 71');
+var latlng = map.getCenter();
+var utmk = naver.maps.TransCoord.fromLatLngToUTMK(latlng); // 위/경도 -> UTMK
+var tm128 = naver.maps.TransCoord.fromUTMKToTM128(utmk);   // UTMK -> TM128
+var naverCoord = naver.maps.TransCoord.fromTM128ToNaver(tm128); // TM128 -> NAVER
+
+infoWindow = new naver.maps.InfoWindow({
+  content: ''
+});
+
+map.addListener('click', function(e) {
+  var latlng = e.coord,
+      utmk = naver.maps.TransCoord.fromLatLngToUTMK(latlng),
+      tm128 = naver.maps.TransCoord.fromUTMKToTM128(utmk),
+      naverCoord = naver.maps.TransCoord.fromTM128ToNaver(tm128);
+
+  utmk.x = parseFloat(utmk.x.toFixed(1));
+  utmk.y = parseFloat(utmk.y.toFixed(1));
+
+  infoWindow.setContent([
+      '<div style="padding:10px;width:380px;font-size:14px;line-height:20px;">',
+      '<strong>LatLng</strong> : '+ '좌 클릭 지점 위/경도 좌표' +'<br />',
+      '<strong>UTMK</strong> : '+ '위/경도 좌표를 UTMK 좌표로 변환한 값' +'<br />',
+      '<strong>TM128</strong> : '+ '변환된 UTMK 좌표를 TM128 좌표로 변환한 값' +'<br />',
+      '<strong>NAVER</strong> : '+ '변환된 TM128 좌표를 NAVER 좌표로 변환한 값' +'<br />',
+      '</div>'
+  ].join(''));
+
+  infoWindow.open(map, latlng);
+  console.log('LatLng: ' + latlng.toString());
+  console.log('UTMK: ' + utmk.toString());
+  console.log('TM128: ' + tm128.toString());
+  console.log('NAVER: ' + naverCoord.toString());
+});
 }
 
 naver.maps.onJSContentLoaded = initGeocoder;
